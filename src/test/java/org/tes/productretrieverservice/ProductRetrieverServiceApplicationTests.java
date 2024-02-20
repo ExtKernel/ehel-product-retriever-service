5f1c4165f9f88d7a3d4fa1f15bab6943582f89ec
exkernel <dev.exkernel@gmail.com>
2024-02-20 17:05:49 +0400

    feat: add tests for context loading

---
+++ src/test/java/org/tes/productretrieverservice/ProductRetrieverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tes.productretrieverservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRetrieverServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
