100bede7cb263c906cca0dd61bea9acdb4c1b14c
exkernel <dev.exkernel@gmail.com>
2024-02-20 17:05:23 +0400

    feat: add main class, security config, cache config

---
+++ src/main/java/org/tes/productretrieverservice/ProductRetrieverServiceApplication.java
@@ -0,0 +1,13 @@
+package org.tes.productretrieverservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRetrieverServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductRetrieverServiceApplication.class, args);
+    }
+
+}
