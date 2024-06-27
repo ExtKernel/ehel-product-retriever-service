518e371a6260dc1086abfef57c739dd38092f94e
exkernel <dev.exkernel@gmail.com>
2024-06-27 11:41:09 +0400

    feat: create a generic CRUD service

---
+++ src/main/java/org/tes/productretrieverservice/exception/ModelNotFoundException.java
@@ -0,0 +1,15 @@
+package org.tes.productretrieverservice.exception;
+
+public class ModelNotFoundException extends RuntimeException {
+    public ModelNotFoundException(String message) {
+        super(message);
+    }
+
+    public ModelNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ModelNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
